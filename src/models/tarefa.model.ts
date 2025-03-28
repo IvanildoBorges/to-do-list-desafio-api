2e385a455db73098bfc78be84f2e5c0ebad5b6b2
IvanildoBorges <ivanildo.rap@gmail.com>
2025-03-28 18:34:24 -0300

    Alterada geração do prisma e import tarefa.model.ts

--- src/models/tarefa.model.ts
+++ src/models/tarefa.model.ts
@@ -1,4 +1,4 @@
-import { Tarefa as TipoGeradoPeloPrisma } from "../generated/client";
+import { Tarefa as TipoGeradoPeloPrisma } from ".prisma/client";
 
 // Modelo de tarefa
 export type Tarefa = TipoGeradoPeloPrisma; // Usa o tipo gerado pelo Prisma
